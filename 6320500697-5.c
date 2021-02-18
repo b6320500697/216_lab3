ff6dc3ac28e991296a3a00ded24825cf4ec48360
NewAlisa <78525157+NewAlisa@users.noreply.github.com>
2021-02-18 16:22:28 +0700

    Luck

---
+++ 6320500697-5.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main ()
+{
+    int n,n2,i,k=0,ch;
+    scanf ("%d",&n);
+    n2=n;
+    for (i=2; ;i++)
+    {
+        if (n2%i==0)
+        {
+            k++;
+        }
+        if (k>3)
+        {
+            printf ("%d is not a Lucky Number.",n);
+        }
+        else
+        {
+             printf ("%d is a Lucky Number",n);
+        }
+    }
+    return 0;
+}
