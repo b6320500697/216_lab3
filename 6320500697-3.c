105b1961f866a96e8abe254d9ed63f21aeadaef5
NewAlisa <78525157+NewAlisa@users.noreply.github.com>
2021-02-18 15:54:19 +0700

    factorization

---
+++ 6320500697-3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main()
+{
+    int n,n2,i=2;
+    scanf ("%d",&n);
+    do
+    {
+        if(n%i==0)
+        {
+            printf("%d\n",i);
+            n=n/i;
+        }
+        else
+        {
+            i++;
+        }
+        if (n==0)
+        {
+            printf ("%d",n);
+            break;
+        }
+    }
+    while (1);
+    return 0;
+}
