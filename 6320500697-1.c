e1889df88a962a9fb36bf065d998cc20f081c24c
NewAlisa <78525157+NewAlisa@users.noreply.github.com>
2021-02-18 15:31:35 +0700

    pet

---
+++ 6320500697-1.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int i,j,k,max=0,win=0,total;
+    for(i=1;i<=5;i++)
+    {
+        total=0;
+        for(j=0;j<4;j++)
+        {
+            scanf("%d" ,&k);
+            total++;
+        }
+        if(total>max)
+        {
+            max=total;
+            win=i;
+        }
+    }
+    printf("%d %d\n" ,win,max);
+
+    return 0;
+}
